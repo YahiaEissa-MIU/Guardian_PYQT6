47cacce847914a4ab1a00d0677291cd2f22073a4
Yahia <yahia2108257@miuegypt.edu.eg>
2025-06-20 12:16:07 +0300

    deleted old

--- models/__init__.py
+++
@@ -1,9 +0,0 @@
-# models/__init__.py
-from .wazuh_config import WazuhConfig
-from .incident_history_model import IncidentHistoryModel
-
-
-__all__ = [
-    'WazuhConfig',
-    'IncidentHistoryModel',
-]
--- utils/__init__.py
+++
@@ -1,8 +0,0 @@
-# utils/__init__.py
-from .alert_manager import AlertManager
-from .config_manager import ConfigManager
-
-__all__ = [
-    'AlertManager',
-    'ConfigManager'
-]
